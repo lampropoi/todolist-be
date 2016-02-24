dbbde879b2b07f7d0b4dbc9bbba89d1f201588c9
lampropoi <lampropoi@gmail.com>
2016-02-24 22:21:12 +0200

    nodemon and node-inspector added

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+/* Routes for our API */
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({message: 'hooray! welcome to our api!'});
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/v1', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
